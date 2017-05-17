9b0692ecf5070b23e9b2e22a356f228c4d4e8f25
Joel Åström <joel_aastroem@hotmail.com>
2017-05-17 21:34:29 +0200

    Added onChange handling for checkbox, added routing to other URI:s

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom"
+
 import "./App.css";
 import { FormHeader } from "./components/FormHeader"
 import { FormInput } from "./components/FormInput"
@@ -7,12 +9,19 @@ import { FormButton } from "./components/FormButton"
 
 class App extends Component {
   state = {
-    textInput: ""
+    textInput: "",
+    isChecked: false
   }
 
   handleInputChange = (evt) => {
     this.setState({
       textInput: evt.target.value
+    })
+  }
+
+  handleCheckboxChange = (evt) => {
+    this.setState({
+      isChecked: !this.state.isChecked
     })
   }
 
@@ -22,21 +31,58 @@ class App extends Component {
 
   render() {
     const headerTexts = [
-      {id: 1, name: "Form"},
-      {id: 2, name: "Review"},
-      {id: 3, name: "Final"}
+      { id: 1, name: "Form" },
+      { id: 2, name: "Review" },
+      { id: 3, name: "Final" }
     ]
 
+    const Review = () => {
+      return (
+        <div>
+          <Link to="/">
+            <FormButton buttonText="Back"></FormButton>
+          </Link>
+          <Link to="/final">
+            <FormButton buttonText="Submit"></FormButton>
+          </Link>
+        </div>
+      )
+    }
+
+    const Final = () => {
+      return (
+        <FormCheckbox handleCheckboxChange={this.handleCheckboxChange} checked={this.state.isChecked} checkBoxText="I agree to the terms of service" />
+      )
+    }
+
     return (
-      <div className="App">
-        <FormHeader headerTexts={headerTexts}></FormHeader>
-        <FormInput handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} labelText="Name"></FormInput>
-        <FormInput handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} labelText="Company"></FormInput>
-        <FormCheckbox checkBoxText="I agree to the terms of service" />
-        <FormButton buttonText="Submit"></FormButton>
-      </div>
+      <Router>
+        <div className="App">
+          <FormHeader headerTexts={headerTexts}></FormHeader>
+          <div className="content">
+            <FormInput handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} labelText="Name"></FormInput>
+            <FormInput handleInputChange={this.handleInputChange} handleSubmit={this.handleSubmit} labelText="Company"></FormInput>
+
+            <Route exact={true} path="/" render={() => (
+              <div>
+                <FormCheckbox handleCheckboxChange={this.handleCheckboxChange} checked={this.state.isChecked} checkBoxText="I agree to the terms of service" />
+                <Link to="/review">
+                  <FormButton buttonText="Review"></FormButton>
+                </Link>
+              </div>
+            )} />
+
+            <Route path="/review" component={Review} />
+            <Route path="/final" component={Final} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
+
+
+
+
 
 export default App;
--- src/components/FormCheckbox.js
+++ src/components/FormCheckbox.js
@@ -4,7 +4,10 @@ export const FormCheckbox = (props) => {
     return (
         <div>
             <label className="form-checkbox-text">{props.checkBoxText}</label>
-            <input className="form-checkbox" type="checkbox" value="None" />
+            <input type="checkbox"
+                className="form-checkbox"
+                onChange={props.handleCheckboxChange}
+                checked={props.checked} />
         </div>
     )
 }
