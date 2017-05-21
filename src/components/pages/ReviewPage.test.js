bca2cdecf754cb4d0d1b42b24339ba4a9409d89c
Joel Åström <joel_aastroem@hotmail.com>
2017-05-21 17:58:22 +0200

    Added unit tests for ReviewPage, added a test case for FinalPage

--- src/components/pages/ReviewPage.test.js
+++ src/components/pages/ReviewPage.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { FinalPage } from "./FinalPage";
+import { ReviewPage } from "./ReviewPage";
 import { shallow } from "enzyme";
 
-describe("When FinalPage renders", () => {
+describe("When ReviewPage renders", () => {
     const appState = {
         userName: "Bob",
         userCompany: "Foo Company"
@@ -12,32 +12,43 @@ describe("When FinalPage renders", () => {
         handleCompanyInputChange: () => { },
         handleSubmit: () => { },
         handleCheckboxChange: () => { },
+        disabled: false
     };
     const wrapper = shallow(
-        <FinalPage
+        <ReviewPage
             appState={appState}
             handleNameInputChange={props.handleNameInputChange}
             handleCompanyInputChange={props.handleCompanyInputChange}
             handleSubmit={props.handleSubmit}
             handleCheckboxChange={props.handleCheckboxChange}
+            disabled={props.disabled}
         />);
 
-    it("it should render two divs", () => {
-        expect(wrapper.find("div").length).toBe(2);
+    it("it should render four divs", () => {
+        expect(wrapper.find("div").length).toBe(4);
     });
     it("it should render one FormHeader", () => {
         expect(wrapper.find("FormHeader").length).toBe(1);
     });
-    it("one of the two rendered divs should be a .content div", () => {
+    it("one of the rendered divs should be a .content div", () => {
         expect(wrapper.find(".content").length).toBe(1);
+    });
+    it("one of the rendered divs should be a .button-section div", () => {
+        expect(wrapper.find(".button-section").length).toBe(1);
+    });
+    it("one of the rendered divs should be a .back-button-section div", () => {
+        expect(wrapper.find(".back-button").length).toBe(1);
     });
     it("it should render two FormInputs", () => {
         expect(wrapper.find("FormInput").length).toBe(2);
     });
-    it("it should render one FormCheckbox", () => {
-        expect(wrapper.find("FormCheckbox").length).toBe(1);
+    it("it should render two FormButtons", () => {
+        expect(wrapper.find("FormButton").length).toBe(2);
     });
-    it("it should not render any FormButtons", () => {
-        expect(wrapper.find("FormButton").length).toBe(0);
+    it("it should render two Links", () => {
+        expect(wrapper.find("Link").length).toBe(2);
+    });
+    it("it should not render a FormCheckbox", () => {
+        expect(wrapper.find("FormCheckbox").length).toBe(0);
     });
 });
