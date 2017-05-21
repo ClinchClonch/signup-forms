7c7850c8383baa68b9440e0af9a4d22cdeaff914
Joel Åström <joel_aastroem@hotmail.com>
2017-05-21 17:43:31 +0200

    Added enzyme & test-utils, added unit tests

---
+++ src/components/pages/FinalPage.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FinalPage } from "./FinalPage";
+import { shallow } from "enzyme";
+
+describe("When FinalPage renders", () => {
+    const appState = {
+        userName: "Bob",
+        userCompany: "Foo Company"
+    };
+    const props = {
+        handleNameInputChange: () => { },
+        handleCompanyInputChange: () => { },
+        handleSubmit: () => { },
+        handleCheckboxChange: () => { },
+    };
+    const wrapper = shallow(
+        <FinalPage
+            appState={appState}
+            handleNameInputChange={props.handleNameInputChange}
+            handleCompanyInputChange={props.handleCompanyInputChange}
+            handleSubmit={props.handleSubmit}
+            handleCheckboxChange={props.handleCheckboxChange}
+        />);
+    
+    it("it should render two divs", () => {
+        expect(wrapper.find("div").length).toBe(2);
+    });
+    it("it should render one FormHeader", () => {
+        expect(wrapper.find("FormHeader").length).toBe(1);
+    });
+    it("one of the two rendered divs should be a .content div", () => {
+        expect(wrapper.find(".content").length).toBe(1);
+    });
+    it("it should render two FormInputs", () => {
+        expect(wrapper.find("FormInput").length).toBe(2);
+    });
+    it("it should render one FormCheckbox", () => {
+        expect(wrapper.find("FormCheckbox").length).toBe(1);
+    });
+});
---
+++ src/lib/utils.test.js
@@ -0,0 +1,11 @@
+import { headerTexts } from "./utils";
+
+test("headerTexts should return an object consisting of the call's arguments", () => {
+    const result = headerTexts("foo", "bar", "baz");
+    const expected = {
+        first: "foo",
+        second: "bar",
+        third: "baz"
+    }
+    expect(result).toEqual(expected);
+});
